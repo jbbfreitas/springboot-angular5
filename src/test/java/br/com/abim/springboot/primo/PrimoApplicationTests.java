6767cfe44ed432b8224473eef78bd3fc0cef4965
João Bosco de Barros Freitas <jbbfreitas@gmail.com>
2018-11-18 10:52:35 -0300

    Commit inicial

---
+++ src/test/java/br/com/abim/springboot/primo/PrimoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.abim.springboot.primo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
